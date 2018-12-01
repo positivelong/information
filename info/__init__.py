4b8bf81b149c46fd4211c00d1992e5506d444396
hua <positivehua@outlook.com>
2018-12-01 22:03:39 +0800

    抽取app创建到函数中&配置名字作为参数

--- info/__init__.py
+++ info/__init__.py
@@ -5,13 +5,19 @@ from flask_wtf import CSRFProtect
 from config import config
 from flask import Flask
 
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(config["development"])
 # 初始化数据库
-db = SQLAlchemy(app)
-# 初始化redis存储对象
-redis_store = StrictRedis(host=config["development"].REDIS_HOST, port=config["development"].REDIS_PORT)
-# 开启当前项目 CSRF 保护，只做服务器验证功能
-CSRFProtect(app)
-Session(app)
+db = SQLAlchemy()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+    # 加载配置
+    app.config.from_object(config[config_name])
+    db.init_app(app)
+    # 初始化redis存储对象
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    # 开启当前项目 CSRF 保护，只做服务器验证功能
+    CSRFProtect(app)
+    Session(app)
+
+    return app
