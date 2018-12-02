62f73dacd4da9539104ee2c81010b27f90b5ae5d
hua <positivehua@outlook.com>
2018-12-02 21:27:06 +0800

    根路径的模板渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,10 @@
+from flask import render_template
+
 from info import redis_store
 from . import index_blu
 
 @index_blu.route('/')
 def index():
     # 向redis中保存一个值 name long
-    redis_store.set("name", "long")
-    return 'index'
+    # redis_store.set("name", "long")
+    return render_template('news/../../static/news/html/../../templates/news/index.html')
